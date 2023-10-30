90c740350a83af5bb3faa500f7a62e9821265c80
Saharsh0 <113522933+Saharsh0@users.noreply.github.com>
2023-10-30 17:37:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ function preload() {
 }
 
 function setup() {
+
 	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
 	instializeInSetup(mario);
 }
 
 function draw() {
-	game()
+	game();
 }
 
 
